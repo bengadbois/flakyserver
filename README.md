3f129b603172f3e71ec5ae84d75922f252735017
Ben Gadbois <enharmonicdistortion@gmail.com>
2017-01-28 03:05:28 +0100

    Initial version with basic functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# Flaky Server
+
+HTTP server that will inconsistently server 200 and 500 responses.
+
+```
+Usage of flakyserver:
+  -errorPercent float
+        Percentage of the requests that should return a server error (default 50)
+  -port int
+        Which port to listen on (default 8899)
+````
---
+++ flakyserver.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+var portPtr *int
+var badPercentPtr *float64
+
+func main() {
+	portPtr = flag.Int("port", 8899, "Which port to listen on")
+	badPercentPtr = flag.Float64("errorPercent", 50, "Percentage of the requests that should return a server error")
+	flag.Parse()
+
+	h := &handler{}
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *portPtr),
+		Handler: h,
+	}
+	log.Println("Starting Flaky Server on " + server.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+type handler struct {
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Receiving request " + r.Method + " " + r.URL.String())
+
+	if rand.Float64()*100 < *badPercentPtr {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Flaky Server had an error"))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Flaky Server succeeded"))
+	}
+}
